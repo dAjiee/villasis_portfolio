fc2042f1ee42ee0132dd8221b3abaf6c2c69ac8c
Alexander Villasis <alexandervaugn@gmail.com>
2024-05-24 08:37:20 +0800

    chore: Refactor HomeInfo component to use useMemo and update currentStage logic

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { arrow } from '../assets/icons';
 
+
+//Template
 const InfoBox = ({ text, link, btnText }) => (
     <div className="info-box">
         <p className="font-medium sm:text-xl text-center">{text}</p>
@@ -13,9 +15,8 @@ const InfoBox = ({ text, link, btnText }) => (
 );
 
 const HomeInfo = ({ currentStage }) => {
-    const [data, setData] = useState({
-        name: ""
-    });
+    //Fetch Data
+    const [data, setData] = useState("");
 
     const fetchData = async () => {
         try {
@@ -28,42 +29,50 @@ const HomeInfo = ({ currentStage }) => {
         }
     };
 
+    //Memo
+    const memo = useMemo(() => {
+            console.log(currentStage);
+            if (currentStage === 1) {
+                return (<h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+                    Hi, I am <span className="font-semibold">{data}!</span>👋
+                    <br />
+                    An Aspiring Software Engineer from Davao City!
+                </h1>);
+            } else if (currentStage === 2) {
+                return (
+                <InfoBox
+                    text="Student at the helm of tech innovation. See how I code and lead."
+                    link="/about"
+                    btnText="Learn More About Me!"
+                />);
+            } else if (currentStage === 3) {
+                return(
+                <InfoBox
+                    text="Fueling innovation with passion. Explore my portfolio of successes."
+                    link="/projects"
+                    btnText="Visit my Portfolio!"
+                />
+                );
+            } else if (currentStage === 4) {
+                return (
+                <InfoBox
+                    text="Need a project done or looking for a dev?"
+                    link="/contact"
+                    btnText="Let's Talk!"
+                />
+                );
+            }
+        ;
+    }, [
+        data,
+        currentStage
+    ]);
+
     useEffect(() => {
         fetchData();
     }, []);
 
-    const renderContent = {
-        1: (
-            <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
-                Hi, I am <span className="font-semibold">Alexander!</span>👋
-                <br />
-                An Aspiring Software Engineer from Davao City!
-            </h1>
-        ),
-        2: (
-            <InfoBox
-                text="Student at the helm of tech innovation. See how I code and lead."
-                link="/about"
-                btnText="Learn More About Me!"
-            />
-        ),
-        3: (
-            <InfoBox
-                text="Fueling innovation with passion. Explore my portfolio of successes."
-                link="/projects"
-                btnText="Visit my Portfolio!"
-            />
-        ),
-        4: (
-            <InfoBox
-                text="Need a project done or looking for a dev?"
-                link="/contact"
-                btnText="Let's Talk!"
-            />
-        ),
-    };
-
-    return renderContent[currentStage] || null;
+    return memo || null;
 };
 
 export default HomeInfo;
