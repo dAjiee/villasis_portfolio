fba858ccff5242a6e9dfdbf6e54ba99e8981d11f
Alexander Villasis <alexandervaugn@gmail.com>
2024-05-25 04:31:16 +0800

    chore: Remove unused sound icons and update image imports

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,9 +1,5 @@
 import arrow from './arrow.svg'
-import soundon from './soundon.png'
-import soundoff from './soundoff.png'
 
 export {
     arrow,
-    soundon,
-    soundoff,
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,28 +6,12 @@ import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
 import HomeInfo from '../components/HomeInfo'
-import sakura from '../assets/sakura.mp3'
-import { soundoff, soundon } from '../assets/icons'
 import tutorialImage from '../assets/tutorial.gif'
 
 const Home = () => {
-  const audioRef = useRef(new Audio(sakura));
-  audioRef.current.volume = 0.4;
-  audioRef.current.loop = true;
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1);
-  const [isPlayingMusic, setisPlayingMusic] = useState(false);
   const [showTutorial, setShowTutorial] = useState(true);
-
-  useEffect(() => {
-    if(isPlayingMusic) {
-      audioRef.current.play();
-    }
-
-    return() => {
-      audioRef.current.pause();
-    }
-  }, [isPlayingMusic])
   
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -100,15 +84,6 @@ const Home = () => {
           />
         </Suspense>
       </Canvas>
-
-      <div className="absolute bottom-2 left-2">
-        <img
-          src={!isPlayingMusic ? soundoff : soundon}
-          alt="sound"
-          className="w-10 h-10 cursor-pointer object-contain"
-          onClick={() => setisPlayingMusic(!isPlayingMusic)}
-        />
-      </div>
 
       {showTutorial && (
         <div className="fade-in-out">
